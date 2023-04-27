94c3f522e4dc2c4ae8ea05fa4a4b10e4d13fbc42
BobrPapaDrevo <115144163+BobrPapaDrevo@users.noreply.github.com>
2023-04-27 11:51:43 +0200

    ship class changes

--- module.ts
+++ module.ts
@@ -1,10 +1,10 @@
 function normalize(x: number, y: number) {
     if (x != 0 && y != 0) {
-        const length = Math.sqrt(Math.pow(Math.abs(x), 2) + Math.pow(Math.abs(y), 2))
+        const length = Math.sqrt(Math.pow(Math.abs(x), 2) + Math.pow(Math.abs(y), 2))//calculates the vector length using the pythagorean theorem
         x /= length
         y /= length
     }else{
-        [x,y] = [Math.sign(x), Math.sign(y)]
+        [x,y] = [Math.sign(x), Math.sign(y)]//this is here because this function returns a number from -1 to 1
     }
-    return [x, y]
+    return [x,y]
 }
